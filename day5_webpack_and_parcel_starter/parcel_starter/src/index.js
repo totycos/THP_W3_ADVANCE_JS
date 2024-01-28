ab9df3582b99a430eae8755d425aefbb1eb28f89
totycos <dv94@hotmail.fr>
2024-01-28 21:01:29 +0100

    Commit ADVANCE_JS day 5 - update webpack.config.js to manage env variable

--- day5_webpack_and_parcel_starter/parcel_starter/src/index.js
+++ day5_webpack_and_parcel_starter/parcel_starter/src/index.js
@@ -1,3 +1,6 @@
 import './style/index.scss';
+import dayjs from 'dayjs';
 
-console.log("Hello Parcel world!");
+console.log("Hello Parcel!");
+console.log(dayjs().format('MMMM DD YYYY')); // January 18 2021
+console.log(dayjs().subtract(10, 'days').format('DD/MM/YYYY')); // 08/01/2021
